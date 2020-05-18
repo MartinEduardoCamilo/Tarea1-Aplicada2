7bb3db0aa81e472f5a87f68c8b7129e3e9e847af
MartinCamilo13 <47596798+MartinCamilo13@users.noreply.github.com>
2020-05-18 12:32:27 -0400

    Terminado

---
+++ Calculadora/Models/Operadores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Models
+{
+    public class Operadores
+    {
+        [Required(ErrorMessage = "El primer campo no puede estar vacía.")]
+        public decimal PrimerNumero { get; set; }
+        [Required(ErrorMessage = "El segundo campo no puede estar vacía.")]
+        public decimal SegundoNumero { get; set; }
+        public decimal resultado { get; set; }
+
+        public Operadores()
+        {
+            PrimerNumero = 0;
+            SegundoNumero = 0;
+            resultado = 0;
+        }
+    }
+}
